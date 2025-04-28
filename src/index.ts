fb1a80dc42b5f63731bad229d33a510d1a4242d1
c7e715d1b04b17683718fb1e8944cc28 <ikki17617@gmail.com>
2025-04-28 15:17:09 +0900

    package.json, index.ts, tsconfig.json, .npmrc, bun.lock: update dependencies and add transaction ID generation endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,46 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import {
+  ClientTransaction,
+  handleXMigration,
+} from "@lami/x-client-transaction-id";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const document = await handleXMigration();
+const transaction = await ClientTransaction.create(document);
+
+const app = new Elysia();
+app.use(swagger());
+
+app.get(
+  "x-client-transaction-id",
+  async (context) => {
+    const {
+      query: { method, pathname },
+    } = context;
+    const xClientTransactionId = await transaction.generateTransactionId(
+      method,
+      pathname
+    );
+    return {
+      "x-client-transaction-id": xClientTransactionId,
+    };
+  },
+  {
+    query: t.Object({
+      method: t.String({
+        examples: ["GET", "POST"],
+      }),
+      pathname: t.String({
+        examples: [
+          "/graphql/HeWHY26ItCfUmm1e6ITjeA/UserTweets",
+          "/graphql/1VOOyvKkiI3FMmkeDNxM9A/UserByScreenName",
+        ],
+      }),
+    }),
+  }
+);
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
